392e8e930ad8bddc0fe339bfbe77dfcde91298fe
Manolo <manolo191202@gmail.com>
2022-04-06 11:00:30 -0400

    création du projet Spring boot

---
+++ src/main/java/com/example/tp3transactionnelle/Tp3TransactionnelleApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp3transactionnelle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3TransactionnelleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp3TransactionnelleApplication.class, args);
+    }
+
+}
